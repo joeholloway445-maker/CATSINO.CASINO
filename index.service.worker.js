09a2274efed6607ce2f27894ad898f4c94f2434e
Joe Holloway <joeholloway445@gmail.com>
2026-07-20 07:48:34 +0000

    chore: republish Pages with mobile play fixes (#71 export)
    Phone build from periliminal-space-html5 artifact (2026-07-20). Includes
    NPC talk gate, lighting floor, plane damp, touch labels, canvas fill.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784288705|18479529';
+const CACHE_VERSION = '1784533556|79664387';
 /** @type {string} */
 const CACHE_PREFIX = 'Periliminal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
